0f8fd9d224d76a066feb888c913654fb1a9bda11
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-05-06 22:24:09 +0700

    Fix Ball movement logic

--- src/ball.rs
+++ src/ball.rs
@@ -30,12 +30,31 @@ impl Ball {
     unsafe fn reset(&mut self, mut owner: BaseNode) {
         owner.set_global_position(Vector2::new(640.0, 30.0));
         owner.set_physics_process(false);
-        self.timer.start(self.timer.get_wait_time());
         owner.set_linear_velocity(Vector2::new(0.0, 0.0));
     }
 
     #[export]
-    unsafe fn _physics_process(&mut self, mut owner: BaseNode) {
-        owner.set_linear_velocity(owner.get_linear_velocity() * 1.25);
+    unsafe fn _process(&mut self, owner: BaseNode, _delta: f64) {
+        if self.timer.is_stopped() {
+            return;
+        }
+
+        let debug_text = format!("time left = {}", self.timer.get_time_left());
+        godot_print!("{}", debug_text);
+        if self.timer.get_time_left() <= 0.1 {
+            godot_print!("Time out!");
+            self.on_timer_timeout(owner);
+        }
+    }
+
+    #[export]
+    unsafe fn _physics_process(&self, mut owner: BaseNode, _delta: f64) {
+        owner.set_linear_velocity(owner.get_linear_velocity() * 1.0025);
+    }
+
+    unsafe fn on_timer_timeout(&mut self, mut owner: BaseNode) {
+        owner.set_linear_velocity(Vector2::new(-500.0, 300.0));
+        owner.set_physics_process(true);
+        self.timer.stop();
     }
 }
